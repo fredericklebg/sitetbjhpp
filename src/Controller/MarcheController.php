3127a63c87adbe8faeab2177b470ddf3e0c66680
Tom Gimat <elspeedcola1@gmail.com>
2020-05-10 23:21:58 +0200

    le marché de gérard du magrai de postegrechine

--- src/Controller/MarcheController.php
+++ src/Controller/MarcheController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Marche;
+use App\Form\MarcheType;
 use App\Repository\ProduitRepository;
+use Doctrine\Common\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MarcheController extends AbstractController
@@ -11,12 +15,40 @@ class MarcheController extends AbstractController
     /**
      * @Route("/marche", name="marche")
      */
-    public function index(ProduitRepository $repository)
+    public function index(Request $request, ManagerRegistry $manager, ProduitRepository $repository)
     {
         $produits = $repository->findAll();
+
         return $this->render('marche/marche.html.twig', [
             'controller_name' => 'MarcheController',
             'produits' => $produits,
+
         ]);
+    }
+
+    /**
+     * @param Request $request
+     * @param ManagerRegistry $manager
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/create-marche", name="marche_create")
+     */
+    public function createMarche(Request $request, ManagerRegistry $manager){
+        $marche = new Marche();
+
+        $form = $this->createForm(MarcheType::class, $marche);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $manager->getManager()->persist($marche);
+            $manager->getManager()->flush();
+            $this->addFlash('success', 'bg le san t\'a créé le marché');
+            return $this->redirectToRoute('marche');
+
+        }
+
+        return $this->render('marche/create.html.twig', [
+            'formMarche' => $form->createView()
+        ]);
+
     }
 }
