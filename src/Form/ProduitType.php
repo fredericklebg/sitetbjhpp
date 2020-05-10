98ca80f03257d5b34c200fc1a9ea6d3c97573cab
Vincent <vincentsd@yahoo.com>
2020-05-10 23:50:42 +0200

    créer produit

--- src/Form/ProduitType.php
+++ src/Form/ProduitType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Produit;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,7 +15,7 @@ class ProduitType extends AbstractType
         $builder
             ->add('name')
             ->add('type')
-            ->add('image')
+            ->add('image',UrlType::class)
         ;
     }
 
