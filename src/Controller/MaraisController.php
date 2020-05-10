29b5f42d52cf4f5ee0fdb36e8846d5201705f5cd
Tom Gimat <elspeedcola1@gmail.com>
2020-05-10 05:42:27 +0200

    couille

--- src/Controller/MaraisController.php
+++ src/Controller/MaraisController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Marais;
+use App\Form\MaraisType;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MaraisController extends AbstractController
@@ -14,6 +18,29 @@ class MaraisController extends AbstractController
     {
         return $this->render('marais/index.html.twig', [
             'controller_name' => 'MaraisController',
+        ]);
+    }
+
+    /**
+     * @param Request $request
+     * @param ManagerRegistry $managerRegistry
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/create-marais",name="marais_create")
+     */
+    public function create(Request $request, ManagerRegistry $managerRegistry){
+        $marais = new Marais();
+
+        $form = $this->createForm(MaraisType::class, $marais);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $managerRegistry->getManager()->persist($marais);
+            $managerRegistry->getManager()->flush();
+            return $this->redirectToRoute('marais');
+        }
+
+        return $this->render('marais/create.html.twig',[
+            'formMarais' => $form->createView()
         ]);
     }
 }
--- src/Form/MaraisType.php
+++ src/Form/MaraisType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Marais;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,6 +13,9 @@ class MaraisType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+        ->add('marais', TextType::class, [
+            'label' => 'Nom du Marais',
+        ])
         ;
     }
 
