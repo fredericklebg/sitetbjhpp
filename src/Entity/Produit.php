a678bf1eabac85490ab362db804f3be5c8c30d3e
Vincent <vincentsd@yahoo.com>
2020-05-11 02:45:18 +0200

    image tamere

--- src/Entity/Produit.php
+++ src/Entity/Produit.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ProduitRepository")
@@ -35,7 +36,9 @@ class Produit
     private $type;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string")
+     *
+     * @Assert\File(mimeTypes={"image/jpeg","image/gif","image/png"})
      */
     private $image;
 
