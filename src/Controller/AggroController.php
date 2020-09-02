767f4d3d40f76b2d1254c7ed9e55f4d1388887e1
Tom Gimat <elspeedcola1@gmail.com>
2020-09-02 02:13:44 +0200

    vivalto lunga, complexe et équilibré

--- src/Controller/AggroController.php
+++ src/Controller/AggroController.php
@@ -12,7 +12,19 @@ class AggroController extends AbstractController
      */
     public function aggro()
     {
-
-        return $this->render('aggro/index.html.twig');
+        $randomPosition1 = rand(0,200);
+        $randomPosition2 = rand(0,200);
+        $randomPosition3 = rand(0,200);
+        $randomPosition4 = rand(0,200);
+        $randomPosition5 = rand(0,200);
+        $randomPosition6 = rand(0,200);
+        return $this->render('aggro/index.html.twig', [
+            'decalmargin1' => $randomPosition1,
+            'decalmargin2' => $randomPosition2,
+            'decalmargin3' => $randomPosition3,
+            'decalmargin4' => $randomPosition4,
+            'decalmargin5' => $randomPosition5,
+            'decalmargin6' => $randomPosition6,
+        ]);
     }
 }
